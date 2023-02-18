eb8f0fc9cf14f1eb280297d0eb86c44f53785316
zbhing <zbhing@qq.com>
2023-02-18 17:47:17 +0800

    fix:补充pom.xml(root):dependencyManagement管理的版本号

--- SRM-DemoV1/src/main/java/com/zbh/SrmApp.java
+++ SRM-DemoV1/src/main/java/com/zbh/SrmApp.java
@@ -1,8 +1,11 @@
 package com.zbh;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author zbhing
  * @description 主启动类
  */
+@SpringBootApplication
 public class SrmApp {
 }
